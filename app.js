ce57e7c924db9ca4d60ef2721a0ca95be9aaf794
fosan <adhityat1201@gmail.com>
2020-11-06 22:34:39 +0700

    cors

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const app = express()
 const port = 8080
 const upload = require('./middlewares/uploadFile')
 const errorHandler = require('./middlewares/errorHandler')
-// const cors = require('cors')
+const cors = require('cors')
 
 
 
-// app.use(cors())
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
